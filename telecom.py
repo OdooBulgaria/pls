d44cdc7fe7847819e65fe495f79fb5abf1fe903c
manjiri <manjiri.kulkarni@jginfosystems.com>
2015-07-18 12:25:47 +0530

    work description functionality completed

--- telecom.py
+++ telecom.py
@@ -10,9 +10,39 @@ class telecom_project(osv.osv):
               'start_date':fields.date(string="Start Date"),
               'end_date':fields.date(string="End Date"),
               'image':fields.binary("Bianry field"),
-              'contact_no':fields.related('customer','phone',type="char",string="Contact No")
+              'contact_no':fields.related('customer','phone',type="char",string="Contact No"),
+              'line_id':fields.one2many('project.description.line','project_id',string="Work Description")
+              
               }
+class project_description_line(osv.osv):
+    _name="project.description.line"  
     
+    def setof_associated_activities(self,cr,uid,ids,description_id,context=None):
+        print "=========================================ids",ids,description_id
+        description=self.pool.get('work.description').browse(cr, uid, description_id,context)
+        values=[]
+        for i in description.activity_ids:
+            values.append((0,0,{'activity_id':i.id}))
+        print "===================values",values
+        return {
+                'value':{'activity_ids':values}
+                 
+               }
+        
+        
+    _columns={
+              'description_id':fields.many2one('work.description',string="Work Description"),
+              'activity_ids':fields.one2many('activity.line','activity_line',string="Activities"),
+              'project_id':fields.many2one('telecom.project')
+              }
+class activity_line(osv.osv):
+    _name='activity.line'
+    _columns={
+              'activity_line':fields.many2one('project.description.line'),
+              'activity_id':fields.many2one('activity.activity',string = "Activity Name"),
+              'cost':fields.float(string='Cost')
+              }
+      
 class telecom_circle(osv.osv):
     _name='telecom.circle'
     _columns={'name':fields.char(string='Circle Name'),
@@ -22,6 +52,7 @@ class telecom_circle(osv.osv):
     
 class work_description(osv.osv):  
     _name="work.description"
+      
     _columns={'name':fields.char(string='Work Description'),
               'activity_ids':fields.many2many('activity.activity',"work_description_activity_activity_rel",'description_id','activity_id',string='Activities'),
               }
